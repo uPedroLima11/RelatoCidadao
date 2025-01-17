e8d56eb13848d1b7a10eaccb53c190f17f481d55
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-01-17 02:24:46 -0300

    ajustes responsividade

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,22 +1,24 @@
 "use client";
 
 import Image from 'next/image';
-
+import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="bg-zinc-900 py-6">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="flex flex-col items-center mb-4 md:mb-0">
-          <Image
-            src="/logo2.png"
-            alt="logo"
-            width={50}
-            height={50}
-            quality={100}
-            priority
-            className="mb-2"
-          />
-          <span className="text-white text-lg font-bold">Relato Cidadão</span>
+          <Link href="/" scroll={false} className="flex flex-col items-center">
+            <Image
+              src="/logo2.png"
+              alt="logo"
+              width={50}
+              height={50}
+              quality={100}
+              priority
+              className="mb-2"
+            />
+            <span className="text-white text-lg font-bold text-center">Relato Cidadão</span>
+          </Link>
         </div>
 
         <div className="text-sm text-white text-center md:text-right">
