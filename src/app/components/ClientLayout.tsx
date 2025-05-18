c321fa04c6df13735ff14de3bd4bf78f472f19c0
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-05-18 00:56:08 -0300

    teste deploy

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -6,39 +6,45 @@ import { useAuth } from "./AuthContext";
 import Navbar from "./Navbar";
 import ListaPostagens from "./ListarPostagens";
 import Footer from "./footer";
+import Script from "next/script";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const [estadoId, setEstadoId] = useState<number | null>(null);
-  const [cidadeId, setCidadeId] = useState<number | null>(null);
+    const [estadoId, setEstadoId] = useState<number | null>(null);
+    const [cidadeId, setCidadeId] = useState<number | null>(null);
 
-  const handleFiltrar = (
-    selectedEstadoId: number | null,
-    selectedCidadeId: number | null
-  ) => {
-    setEstadoId(selectedEstadoId);
-    setCidadeId(selectedCidadeId);
-  };
+    const handleFiltrar = (
+        selectedEstadoId: number | null,
+        selectedCidadeId: number | null
+    ) => {
+        setEstadoId(selectedEstadoId);
+        setCidadeId(selectedCidadeId);
+    };
 
-  const handleRemoverFiltro = () => {
-    setEstadoId(null);
-    setCidadeId(null);
-  };
+    const handleRemoverFiltro = () => {
+        setEstadoId(null);
+        setCidadeId(null);
+    };
 
-  const pathname = usePathname();
-  const { isAuthenticated } = useAuth();
+    const pathname = usePathname();
+    const { isAuthenticated } = useAuth();
 
-  const criarPostagem = pathname === "/postagem";
-  const minhasPostagensPage = pathname === "/minhasPostagens";
-  const postagemDetalhePage = pathname.startsWith("/postagens/");
+    const criarPostagem = pathname === "/postagem";
+    const minhasPostagensPage = pathname === "/minhasPostagens";
+    const postagemDetalhePage = pathname.startsWith("/postagens/");
 
-  return (
-    <>
-      <Navbar onFiltrar={handleFiltrar} onRemoverFiltro={handleRemoverFiltro} />
-      {isAuthenticated && !criarPostagem && !minhasPostagensPage && !postagemDetalhePage && (
-        <ListaPostagens estadoId={estadoId} cidadeId={cidadeId} />
-      )}
-      {children}
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                id="hs-script-loader"
+                src="https://js-na1.hs-scripts.com/50069658.js"
+            />
+            <Navbar onFiltrar={handleFiltrar} onRemoverFiltro={handleRemoverFiltro} />
+            {isAuthenticated && !criarPostagem && !minhasPostagensPage && !postagemDetalhePage && (
+                <ListaPostagens estadoId={estadoId} cidadeId={cidadeId} />
+            )}
+            {children}
+            <Footer />
+        </>
+    );
 }
